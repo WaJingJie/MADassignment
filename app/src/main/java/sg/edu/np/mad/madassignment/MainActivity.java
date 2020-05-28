a89c6afdd7087c66fe16537ca77b83cad51075b3
Jing Jie Wang <s10198179@connect.np.edu.sg>
2020-05-28 23:07:45 +0800

    tweaked contraint for both loginpage and activity_main to work. added MainPage and Login page for class. Did a placeholder intent from activity_main to loginpage (login button).
    ps. background picture can be changed via the layout.xml code (android:background:"")

--- app/src/main/java/sg/edu/np/mad/madassignment/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/MainActivity.java
@@ -2,13 +2,26 @@ package sg.edu.np.mad.madassignment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        loginbtn = findViewById(R.id.welcomelogin);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginpage = new Intent(MainActivity.this, LoginPage.class);
+                startActivity(loginpage);
+            }
+        });
     }
 }
