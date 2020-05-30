14b2b2b2e944807ca2b4b12718497ae07dffdcd6
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-05-30 21:59:28 +0800

    edited login page

--- app/src/main/java/sg/edu/np/mad/madassignment/LoginPage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/LoginPage.java
@@ -2,7 +2,6 @@ package sg.edu.np.mad.madassignment;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.os.Bundle;
 import android.util.Log;
@@ -30,51 +29,42 @@ public class LoginPage extends AppCompatActivity {
         submitbtn = findViewById(R.id.loginsubmit);
         cancelbtn = findViewById(R.id.logincancel);
 
-
-        dbHandler = new DBHandler(this);
-
         submitbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String _username = username.getText().toString();
-                String _password = password.getText().toString();
-
-                Log.d(TAG, FILENAME + format(": New user creation with %s: %s",_username,_password));
-
-                if (dbhandler.findUser(_username) != null){
-                    Log.d(TAG, "User already exist during new user creation.");
-                    resetText();
+                String email = loginemail.getText().toString();
+                String password = loginpassword.getText().toString();
+                Log.v(TAG, FILENAME + ": Logging in with: " + email + ": " + password);
+                if (userValid(email, password) == false){
+                    Log.v(TAG, FILENAME + ": Invalid user!");
+                    reset();
+                    Toast.makeText(getApplicationContext(), "Invalid email or password! Please re-enter again.",
+                            Toast.LENGTH_LONG).show();
                     return;
                 }
 
-                ArrayList<Integer> initial_level_list = new ArrayList<>();
-                ArrayList<Integer> initial_score_list = new ArrayList<>();
-                initial_list(initial_score_list, initial_level_list);
-
-                UserData data = new UserData(_username, _password, initial_level_list, initial_score_list);
-                dbhandler.addUser(data);
-                Log.v(TAG, FILENAME + ": New user created successfully!");
-                resetText();
-
-                // redirect to login page
-                Log.v(TAG, FILENAME + ": Redirect to login page");
-                Intent activityName = new Intent(Main2Activity.this, MainActivity.class);
-                startActivity(activityName);
-                Toast.makeText(Main2Activity.this, "The account is created successfully.", Toast.LENGTH_SHORT).show();
-
-
+                // redirect to level select page
             }
         });
 
-        cancel_btn.setOnClickListener(new View.OnClickListener() {
+        cancelbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.v(TAG, FILENAME + ": Redirect to login page");
-                Intent redirect_to_login = new Intent(Main2Activity.this, MainActivity.class);
-                startActivity(redirect_to_login);
-                finish();
+                Intent welcomepage = new Intent(LoginPage.this, MainActivity.class);
+                startActivity(welcomepage);
+                reset();
             }
         });
+
     }
+
+    public boolean userValid(String e, String p){
+        return true;
+    }
+    public void reset(){
+        loginemail.setText("Enter Your Email");
+        loginpassword.setText("Enter Your Password");
+    }
+
 }
