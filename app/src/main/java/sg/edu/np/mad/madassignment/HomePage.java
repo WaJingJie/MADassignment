1678b4e9a2fcfae5d579b83418d1fe4b379b69b7
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-06-03 21:25:18 +0800

    homepage

--- app/src/main/java/sg/edu/np/mad/madassignment/HomePage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/HomePage.java
@@ -1,21 +1,42 @@
 package sg.edu.np.mad.madassignment;
-
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Adapter;
+import android.widget.Button;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class HomePage extends AppCompatActivity {
+    Button addbook;
+    RecyclerView rv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        ArrayList<String> data = new ArrayList<>();
+        rv = findViewById(R.id.homepageview);
+        addbook = findViewById(R.id.homepageadd);
+        LibraryAdapter adapter = new LibraryAdapter(data);
+        rv.setAdapter(adapter);
+        LinearLayoutManager layout = new LinearLayoutManager(this);
+        rv.setLayoutManager(layout);
+        rv.setItemAnimator(new DefaultItemAnimator());
+        addbook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent borrowpage = new Intent(HomePage.this, BorrowBook.class);
+                startActivity(borrowpage);
+            }
+        });
     }
 
-    public void onClick(View v){
-        EditText input = findViewById(R.id.enterisbn);
-        String txt = input.getText().toString();
-    }
 }
