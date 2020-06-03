86c067d5bdd4f9470618f6b8fade8bd0e991e1fc
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-06-03 17:43:57 +0800

    updated dbhandler

--- app/src/main/java/sg/edu/np/mad/madassignment/DBHandler.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/DBHandler.java
@@ -8,88 +8,92 @@ import android.util.Log;
 
 import java.util.ArrayList;
 public class DBHandler extends SQLiteOpenHelper{
-    private static final int DATABASE_VERSION = 1;
     private static final String FILENAME = "DBHandler.java";
     public static final String DATABASE_NAME = "NPLibrary.db";
+    private static final int DATABASE_VERSION = 1;
     private static final String TAG = "NP Library!";
-    public static final String TABLE_PRODUCTS = "products";
-    public static final String COLUMN_ID = "_id";
-    public static final String COLUMN_PRODUCTNAME = "productname";
-    public static final String COLUMN_QUANTITY = "quantity";
-
+    public static final String TABLE_USERDATA= "users";
+    public static final String COLUMN_EMAIL = "useremail";
+    public static final String COLUMN_NAME = "username";
+    public static final String COLUMN_PASSWORD = "userpassword";
+    public static final String COLUMN_NO_OF_BOOKS_BORROWED= "noofbooksborrowed";
+    public static final String COLUMN_NO_OF_BOOKS_USER_CAN_BORROW = "canborrow";
     public DBHandler(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, DATABASE_NAME, factory, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_PRODUCTS_TABLE = "CREATE TABLE " + TABLE_PRODUCTS +
-                (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-                + COLUMN_PRODUCTNAME
-                + " TEXT," + COLUMN_QUANTITY + " INTEGER" + ")";
-        db.execSQL(CREATE_PRODUCTS_TABLE);
+        String CREATE_USERDATA_TABLE = "CREATE TABLE " + TABLE_USERDATA +
+                "(" + COLUMN_EMAIL + "TEXT," + COLUMN_NAME
+                + " TEXT," + COLUMN_PASSWORD + " TEXT,"
+                + COLUMN_NO_OF_BOOKS_BORROWED + "INTEGER,"
+                + COLUMN_NO_OF_BOOKS_BORROWED + "INTEGER" + ")";
+        db.execSQL(CREATE_USERDATA_TABLE);
+        Log.v(TAG, "DB Created: " + CREATE_USERDATA_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_USERDATA);
         onCreate(db);
     }
 
     public void addUser(UserData userData) {
         ContentValues values = new ContentValues();
-            values.put(COLUMN_PRODUCTNAME, userData.getProductName());
-        values.put(COLUMN_QUANTITY, userData.getQuantity());
+        values.put(COLUMN_EMAIL, userData.getMyEmail());
+        values.put(COLUMN_NAME, userData.getMyName());
+        values.put(COLUMN_PASSWORD, userData.getMyPassword());
+        values.put(COLUMN_NO_OF_BOOKS_BORROWED, userData.getBooksBorrowed());
+        values.put(COLUMN_NO_OF_BOOKS_USER_CAN_BORROW, userData.getCanborrow());
         SQLiteDatabase db = this.getWritableDatabase();
-        db.insert(TABLE_PRODUCTS, null, values);
+        db.insert(TABLE_USERDATA, null, values);
         db.close();
         Log.v(TAG, FILENAME + ": Adding data for Database: " + values.toString());
     }
 
     public UserData findUser(String email) {
-        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE "
-                + COLUMN_PRODUCTNAME
-                + " = \"" + productname + "\"";
-
+        String query = "SELECT * FROM " + TABLE_USERDATA + " WHERE "
+                + COLUMN_EMAIL
+                + " = \"" + email + "\"";
         SQLiteDatabase db = this.getWritableDatabase();
-
         Cursor cursor = db.rawQuery(query, null);
-
         UserData userData = new UserData();
-
         Log.v(TAG, FILENAME +": Find user form database: " + query);
 
         if(cursor.moveToFirst()){
-            product.setID(Integer.parseInt(cursor.getString(0)));
-            product.setProductName(cursor.getString(1));
-            product.setQuantity(Integer.parseInt(cursor.getString(2)));
+            userData.setMyEmail(cursor.getString(0));
+            userData.setMyName(cursor.getString(1));
+            userData.setMyPassword(cursor.getString(2));
+            userData.setBooksBorrowed(Integer.parseInt(cursor.getString(3)));
+            userData.setCanborrow(Integer.parseInt(cursor.getString(4)));
             cursor.close();
-            Log.v(TAG, FILENAME + ": QueryData: " + queryData.getLevels().toString() + queryData.getScores().toString());
+            Log.v(TAG, FILENAME + ": QueryData: " + userData.getBooksBorrowed() + userData.getCanborrow());
         }
         else{
             Log.v(TAG, FILENAME+ ": No data found!");
         }
         db.close();
-        return email;
+        return userData;
 
     }
 
-    public boolean deleteAccount(String username) {
+    public boolean deleteAccount(String email) {
         boolean result = false;
 
-        String query = "SELECT * FROM " + TABLE_PRODUCTS + " WHERE "
-                + COLUMN_PRODUCTNAME + " = \""
-                + productname + "\"";
+        String query = "SELECT * FROM " + TABLE_USERDATA + " WHERE "
+                + COLUMN_EMAIL + " = \""
+                + email + "\"";
         SQLiteDatabase db = this.getWritableDatabase();
 
         Cursor cursor = db.rawQuery(query, null);
 
-        Product product = new Product();
+        UserData userData = new UserData();
         if (cursor.moveToFirst()) {
-            product.setID(Integer.parseInt(cursor.getString(0)));
-            db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?",
-                    new String[] { String.valueOf(product.getID()) });
+            userData.setMyEmail(cursor.getString(0));
+            db.delete(TABLE_USERDATA, COLUMN_EMAIL + " = ?",
+                    new String[] { String.valueOf(userData.getMyEmail()) });
             cursor.close();
             result = true;
         }
