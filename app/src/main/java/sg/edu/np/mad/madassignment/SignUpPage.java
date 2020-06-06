4ad1fc81f696807cdab4f680e8dd9ca2d8cfbaf7
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-06-06 16:28:38 +0800

    documented sign up page

--- app/src/main/java/sg/edu/np/mad/madassignment/SignUpPage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/SignUpPage.java
@@ -33,6 +33,8 @@ public class SignUpPage extends AppCompatActivity {
         submitbutton = findViewById(R.id.signupsubmit);
         cancelbutton = findViewById(R.id.signupcancel);
 
+        //This method is used when the submit button is clicked by the user
+        Log.v(TAG, FILENAME + ": Submit Button Clicked");
         submitbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,11 +63,13 @@ public class SignUpPage extends AppCompatActivity {
             }
         });
 
-
+        //This method is used when the cancel button is clicked by the user
+        Log.v(TAG, FILENAME + ": Cancel Button Clicked");
         cancelbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.v(TAG, FILENAME + ": Return to welcome page");
+                //This intent redirects the user back to the welcome page
                 Intent welcomepage = new Intent(SignUpPage.this, MainActivity.class);
                 startActivity(welcomepage);
                 reset();
@@ -74,24 +78,31 @@ public class SignUpPage extends AppCompatActivity {
         });
     }
 
+    //This method is to check whether the email has been already used by an existing user
 
     public void dupvalidate(String e, String n, String p, int b, int c){
-        DBHandler dbHandler = new DBHandler(this, null, null, 1);
-        UserData data = dbHandler.findUser(e);
 
+        DBHandler dbHandler = new DBHandler(this, null, null, 1);
+        //This searches the database for the entered email
+        Log.v(TAG, FILENAME + ": Searching database for email!");
+        UserData data = dbHandler.findUser(e);
+        //This occurs when the email has been found in the database
         if(data != null){
             Toast.makeText(SignUpPage.this, "User already exists! Please enter another email.",
                     Toast.LENGTH_LONG).show();
         }
+        //This occurs when the email has not been found in the database
         else{
 
             UserData userdata = new UserData(e, n, p, b, c);
+            //This adds the new user information into the database
             dbHandler.addUser(userdata);
-            Log.v(TAG, FILENAME + ": New user successfully created !");
+            Log.v(TAG, FILENAME + ": New user successfully created!");
             Toast.makeText(SignUpPage.this, "Account created successfully!",
                     Toast.LENGTH_SHORT).show();
+            //This saves the data into the public static Userdata object
             LoginPage.userdata = data;
-            //redirect to home page
+            //This redirects the user to the home page
             Log.v(TAG, FILENAME + ": Go to home page");
             Intent homepage = new Intent(SignUpPage.this, HomePage.class);
             startActivity(homepage);
@@ -99,7 +110,9 @@ public class SignUpPage extends AppCompatActivity {
         }
     }
 
+    // This method resets the text in the textboxes in the Sign Up page
     public void reset(){
+        Log.v(TAG, FILENAME + ": Resetting Sign Up page");
         signupemail.setHint("Enter Your Email");
         signupname.setHint("Enter Your Name");
         signuppassword.setHint("Enter Your Password");
