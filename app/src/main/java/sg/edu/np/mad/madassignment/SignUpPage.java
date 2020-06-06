0cdb299e71356106a5d6e62c1f28eff11207e325
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-06-06 21:40:02 +0800

    commented on login page

--- app/src/main/java/sg/edu/np/mad/madassignment/SignUpPage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/SignUpPage.java
@@ -5,12 +5,10 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
 
 public class SignUpPage extends AppCompatActivity {
     private static final String FILENAME = "LoginPage.java";
