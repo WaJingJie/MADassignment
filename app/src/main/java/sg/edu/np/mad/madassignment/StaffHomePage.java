84b342d4737a9a93bfaeb4487781e3ffbf7eb2e2
Jing Ting Megan Png <s10196822@connect.np.edu.sg>
2020-07-21 23:36:37 +0800

    edited

--- app/src/main/java/sg/edu/np/mad/madassignment/StaffHomePage.java
+++ app/src/main/java/sg/edu/np/mad/madassignment/StaffHomePage.java
@@ -1,4 +1,149 @@
 package sg.edu.np.mad.madassignment;
 
-class StaffHomePage {
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SearchView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ListViewCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StaffHomePage extends AppCompatActivity{
+    RecyclerView rv;
+    RecyclerView.LayoutManager layoutManager;
+    Searchbookadapter adapter;
+
+    MaterialSearchBar materialSearchBar;
+
+    List<String> suggestion = new ArrayList<>();
+    List<book> booklist = new ArrayList<>();
+    ImageButton logoutbutton, homebutton, profilebutton, viewbutton, overduebutton;
+    DBHandler dbHandler;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.staffhomepage);
+
+        //initialize database
+        dbHandler = new DBHandler(this,null,null,1);
+
+
+        logoutbutton = findViewById(R.id.studentlogoutbutton);
+        homebutton = findViewById(R.id.studenthomebutton);
+        profilebutton = findViewById(R.id.studentprofilebutton);
+        viewbutton = findViewById(R.id.viewborrowicon);
+        overduebutton = findViewById(R.id.overdueicon);
+
+        materialSearchBar = findViewById(R.id.staffsearchview);
+
+        rv = findViewById(R.id.staffRV);
+        layoutManager = new LinearLayoutManager(this);
+        rv.setLayoutManager(layoutManager);
+        rv.setHasFixedSize(true);
+
+        //search bar setup
+        materialSearchBar.setHint("Search");
+        materialSearchBar.setCardViewElevation(10);
+        loadSuggestList();
+
+        materialSearchBar.addTextChangeListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<String> suggest = new ArrayList<>();
+                for(String search:suggestion){
+                    if(search.toLowerCase().contains(materialSearchBar.getText().toLowerCase())){
+                        suggest.add(search);
+                    }
+                    materialSearchBar.setLastSuggestions(suggest);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        materialSearchBar.setOnSearchActionListener(new MaterialSearchBar.OnSearchActionListener() {
+            @Override
+            public void onSearchStateChanged(boolean enabled) {
+                if(!enabled){
+                    rv.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onSearchConfirmed(CharSequence text) {
+                startSearch(text.toString());
+            }
+
+            @Override
+            public void onButtonClicked(int buttonCode) {
+
+            }
+        });
+        adapter = new Searchbookadapter(this,dbHandler.getBook());
+        rv.setAdapter(adapter);
+
+        //this is to allow the user to log out
+        logoutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent welcomepage = new Intent(StaffHomePage.this, MainActivity.class);
+                startActivity(welcomepage);
+            }
+        });
+
+        //this is to redirect the user to the profile page
+        profilebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent profilepage = new Intent(StaffHomePage.this, ProfilePage.class);
+                startActivity(profilepage);
+            }
+        });
+
+        viewbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewpage = new Intent(StaffHomePage.this, HomePage.class);
+                startActivity(viewpage);
+            }
+        });
+
+        overduebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent overduepage = new Intent(StaffHomePage.this, OverduePage.class);
+                startActivity(overduepage);
+            }
+        });
+    }
+
+    private void loadSuggestList(){
+        suggestion = dbHandler.getBookname();
+        materialSearchBar.setLastSuggestions(suggestion);
+    }
+
+    private void startSearch(String text){
+        adapter = new Searchbookadapter(this, dbHandler.getBookByName(text));
+        rv.setAdapter(adapter);
+    }
 }
